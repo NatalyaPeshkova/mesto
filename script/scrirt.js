1e43dc7720b89120edbe415606c880a38d2144bb
Natasha <fros.frost2010@yandex.ru>
2022-04-11 09:40:50 +1000

    do.то что в инпут, то и на странице

--- script/scrirt.js
+++ script/scrirt.js
@@ -1,7 +1,10 @@
+// открытие и закрытие попапа
 let popupContainer = document.querySelector('.popup__container');
 let popup = document.querySelector('.popup');
 let popupOpenBtn = document.querySelector('.profile-info__edit-button');
 let popupCloseBtn = document.querySelector('.popup__close-button');
+
+
 
 function popupOpenButton() {
   popup.classList.toggle('popup_opened');
@@ -11,3 +14,29 @@ function popupOpenButton() {
 
 popupOpenBtn.addEventListener('click', popupOpenButton);
 popupCloseBtn.addEventListener('click', popupOpenButton);
+
+
+
+//  имя в профайле должно быть равно инпуту в попапе
+
+//  получить кнопку. выбрать кнопку из документ, добавить собтие клик. задать имя функции.
+document.querySelector('.popup__button').onclick = BtnClick;
+
+
+function BtnClick(){
+  console.log('done');
+  // получить инпут
+  let userName = document.querySelector('.popup__text').value;
+  console.log(userName);
+
+  //место для записи инпута .profile-info__text
+  document.querySelector('.profile-info__text').innerHTML = userName;
+  console.log(userName + 'from profile');
+
+
+  popup.classList.toggle('popup_opened');
+  popupContainer.classList.toggle('popup_opened');
+  popupCloseBtn.classList.toggle('popup_opened');
+}
+
+
