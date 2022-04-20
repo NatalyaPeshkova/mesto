03fbc6b93dae958dd94023be52a753893c9cc286
Natasha <fros.frost2010@yandex.ru>
2022-04-20 14:56:23 +1000

    fix.23is53done

--- scripts/scrirts.js
+++ scripts/scrirts.js
@@ -1,9 +1,10 @@
+// хорошая шутка про "последние 53 ошибки". я очень Вас понимаю.
 
 
-let popupContainer = document.querySelector('.popup-container');
+let popupContainer = document.querySelector('.popup__container');
 let popup = document.querySelector('.popup');
 let popupOpenBtn = document.querySelector('.profile__edit-button');
-let popupCloseBtn = document.querySelector('.popup-container__close-button');
+let popupCloseBtn = document.querySelector('.popup__close-button');
 
 let userName1 = document.querySelector('.profile__text').innerHTML;
 let userJob1 = document.querySelector('.profile__data').innerHTML;
@@ -13,8 +14,8 @@ let userJob1 = document.querySelector('.profile__data').innerHTML;
 function popupOpenButton(){
   popup.classList.toggle('popup_opened');
   popupContainer.classList.toggle('popup_opened');
-  document.querySelector('.popup-container__text').value = userName1;
-  document.querySelector('.popup-container__text-info').value = userJob1;
+  document.querySelector('.popup__text').value = userName1;
+  document.getElementById('job').value = userJob1;
 }
 
 popupOpenBtn.addEventListener('click', popupOpenButton);
@@ -23,9 +24,9 @@ popupCloseBtn.addEventListener('click', popupOpenButton);
 
 // функция получет текст в полях ввода попапа и меняет текст в профиле
 
-let formElement = document.querySelector('.popup-container__button');
-let nameInput = document.querySelector('.popup-container__text');
-let jobInput = document.querySelector('.popup-container__text-info');
+let formElement = document.querySelector('.popup__button');
+let nameInput = document.querySelector('.popup__text');
+let jobInput = document.getElementById('job');
 // console.log(nameInput.value + "   рабоатет");
 // console.log(jobInput.value + "    рабоатет ")
 
