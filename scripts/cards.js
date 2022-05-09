580f06e97c4277497aa1f0eff88b5e08c3477135
Natasha <fros.frost2010@yandex.ru>
2022-05-09 23:58:35 +1000

    сделать плавное открытие попапа

--- scripts/cards.js
+++ scripts/cards.js
@@ -100,8 +100,7 @@ document.querySelectorAll('.elements img').forEach(image => {
   image.onclick = () => {
     document.querySelector('.popup-image').style.display = 'block';
     document.querySelector('.popup-image img').src = image.getAttribute('src');
-    document.querySelector('.popup-image__text').value = inputLocationName;
-
+    document.querySelector('.popup-image__text').textContent = image.getAttribute('h2');
   }
 });
 
